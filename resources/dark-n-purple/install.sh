05d26f539bf05306be88098235aa6995cf99dbb0
SuperS123 <57376381+SuperS123@users.noreply.github.com>
2021-03-03 23:08:09 -0500

    Update install.sh

--- resources/dark-n-purple/install.sh
+++ resources/dark-n-purple/install.sh
@@ -1,7 +1,7 @@
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts
 rm -rf main.css
-echo "@import url('https://oreokitten.github.io/latest/dark-n-purple/user.css')" >> /var/www/pterodactyl/resources/scripts/main.css
+echo "@import url('https://raw.githubusercontent.com/SuperS123/PteroThemes/main/1.2.2/Dark-n-Purple/user.css')" >> /var/www/pterodactyl/resources/scripts/main.css
 rm -rf index.tsx
 wget https://raw.githubusercontent.com/DeveloperNeon/ptero-1.0-theme-install/main/resources/index.tsx
 cd ../..
